7eff5b1f232449beb62d08e8b54673082254dc6f
SHAMRA <mohamedsaamran4@gmail.com>
2020-04-19 20:44:13 -0700

    Scake4u

---
+++ app/src/main/java/com/example/cake4u/model/User.java
@@ -0,0 +1,53 @@
+package com.example.cake4u.model;
+
+import android.graphics.Bitmap;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String mail;
+    private String password;
+    private Bitmap bitmap;
+
+    public User() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBitmap(Bitmap bitmap){this.bitmap=bitmap;}
+
+    public Bitmap getImage(){return bitmap;}
+}
+
