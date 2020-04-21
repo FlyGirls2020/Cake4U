cb664f0613f76fa9aaf465e7c7554989defd1f01
IT18181852 <kajany22@gmail.com>
2020-04-21 13:12:57 +0530

    kcake

--- app/src/main/java/com/example/cake4u/SellerRegisterActivity.java
+++ app/src/main/java/com/example/cake4u/SellerRegisterActivity.java
@@ -1,14 +1,112 @@
 package com.example.cake4u;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cake4u.Database.DBHelper;
+import com.example.cake4u.Prevelent.Prevelent;
 
 public class SellerRegisterActivity extends AppCompatActivity {
+
+    DBHelper db;
+
+    EditText userName, eMail, password;
+    Button Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_register);
+
+        userName = findViewById(R.id.seller_username);
+        eMail = findViewById(R.id.seller_email);
+        password = findViewById(R.id.seller_password);
+        Register = findViewById(R.id.seller_register);
+
+        db = new DBHelper(this);
+
+        check_seller_register();
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (emptyCheck()) {
+                    boolean check = db.check_insert_Seller(userName.getText().toString(), eMail.getText().toString());
+                    if (check) {
+                        boolean insert = db.insert_Seller(userName.getText().toString(), eMail.getText().toString(), password.getText().toString(), Prevelent.currentUser.getId());
+                        if (insert) {
+                            Toast.makeText(getApplicationContext(), "Registered successfully", Toast.LENGTH_SHORT).show();
+                            clearText();
+                            Intent intent = new Intent(SellerRegisterActivity.this, SellerAddCakeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to register. Please try again", Toast.LENGTH_SHORT).show();
+                            userName.isFocused();
+                        }
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "You are already a seller!!", Toast.LENGTH_SHORT).show();
+                        userName.isFocused();
+                    }
+                }
+            }
+        });
+    }
+
+    private void check_seller_register() {
+
+        String isApproved=db.retrive_seller_info(Prevelent.currentUser.getId());
+
+        if(isApproved.equals("0")){
+
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("Notification").setMessage("Dear customer please wait for admin approval").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(SellerRegisterActivity.this, AppHomeActivity.class);
+                    startActivity(intent);
+                }
+            }).setCancelable(false).show();
+
+
+        }
+        else if(isApproved.equals("1")){
+            Intent intent = new Intent(SellerRegisterActivity.this, SellerAddCakeActivity.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "You are already registered ..", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean emptyCheck() {
+        if (userName.getText().toString().isEmpty() || eMail.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+            if (userName.getText().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Enter user name", Toast.LENGTH_SHORT).show();
+                userName.isFocused();
+            } else if (eMail.getText().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Enter valid email", Toast.LENGTH_SHORT).show();
+                eMail.isFocused();
+            } else {
+                Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+                password.isFocused();
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void clearText() {
+        userName.getText().clear();
+        eMail.getText().clear();
+        password.getText().clear();
+        userName.isFocused();
     }
 }
--- app/src/main/java/com/example/cake4u/Seller_approval.java
+++ app/src/main/java/com/example/cake4u/Seller_approval.java
@@ -1,14 +1,131 @@
 package com.example.cake4u;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import com.example.cake4u.Adapters.cuslistAdapter;
+import com.example.cake4u.Adapters.sellerAdapter;
+import com.example.cake4u.Database.DBHelper;
+import com.example.cake4u.model.User;
+import com.example.cake4u.model.sellers;
+
+import java.util.ArrayList;
 
 public class Seller_approval extends AppCompatActivity {
+    private DBHelper db;
 
+    private SwipeMenuListView listViews;
+    private ArrayList<sellers> dataArrayList;
+    private sellerAdapter listAdapter;
+    private sellers datas;
+    private Button back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_approval);
+        db=new DBHelper(this);
+        back_btn=findViewById(R.id.R_admin_view_customer_back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Seller_approval.this,AdminHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        listViews = (SwipeMenuListView) findViewById(R.id.R_admin_view_customer_details1);
+        dataArrayList = new ArrayList<>();
+
+        retrive_customer_details();
     }
+
+    private void retrive_customer_details() {
+
+        dataArrayList=db.Retrive_seller_allDetails();
+        listAdapter = new sellerAdapter(this, dataArrayList);
+        listViews.setAdapter(listAdapter);
+
+        listViews.setMenuCreator(creator);
+
+        listViews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> av, View view, final int position, long l) {
+                AlertDialog.Builder builder =new AlertDialog.Builder(Seller_approval.this);
+                builder.setTitle("Approval").setMessage("Do you want to approve current seller").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        boolean isUpdated=db.update_seller_approval(dataArrayList.get(position).getId());
+                        if(isUpdated){
+                            Toast.makeText(Seller_approval.this,"Seller approved",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(Seller_approval.this,AdminHomeActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setCancelable(false).show();
+
+            }
+        });
+
+        listViews.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Toast.makeText(Seller_approval.this,"Sucessfully deleted",Toast.LENGTH_SHORT).show();
+                        delete_cur_customer(position);
+                        dataArrayList.remove(position);
+                        listAdapter.notifyDataSetChanged();
+                        break;
+                    case 1:
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+
+    private void delete_cur_customer(int position) {
+        boolean isDeleted=db.admin_delete_seller(dataArrayList.get(position).getId());
+
+    }
+
+    SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+        @Override
+        public void create(SwipeMenu menu) {
+            // create "delete" item
+            SwipeMenuItem deleteItem = new SwipeMenuItem(
+                    getApplicationContext());
+            // set item background
+            deleteItem.setBackground(new ColorDrawable(Color.parseColor("#F45557")));
+            // set item width
+            deleteItem.setWidth(150);
+            deleteItem.setIcon(R.drawable.user_admin_delete_icon);
+            deleteItem.setTitleColor(Color.WHITE);
+
+            // add to menu
+            menu.addMenuItem(deleteItem);
+        }
+    };
 }
+
